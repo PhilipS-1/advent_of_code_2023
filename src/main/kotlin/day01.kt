c23c9ce7a50b24ac00a84d830a654356c28acbe4
Philip <philipsandmann@gmail.com>
2023-12-01 17:20:49 +0100

    better solution

--- src/main/kotlin/day01.kt
+++ src/main/kotlin/day01.kt
@@ -13,43 +13,29 @@ fun main(){
 }
 
 fun star1(input: List<String>): Int{
-    return input.map { it.filter { it.isDigit() } }
+    return input
+        .map { it.filter { it.isDigit() } }
         .map{ it.first().toString() + it.last()}
         .fold(0) {acc, line -> acc + line.toInt()}
 }
 
-fun star2(input: List<String>): Int{
-    return input.map {findFirstNumber(it).toString() + findLastNumber(it)}
-        .fold(0) {acc, line -> acc + line.toInt()}
+
+fun star2(input: List<String>): Int {
+    return input
+        .map {mapNumbers(it)}
+        .map{ it.filter { it.isDigit() }}
+        .map{ it.first().toString() + it.last()}
+        .fold(0) { acc, line -> acc + line.toInt() }
 }
 
-fun findFirstNumber(s: String): Char =
-    when {
-        s.first().isDigit() -> s.first()
-        s.startsWith("one") -> '1'
-        s.startsWith("two") -> '2'
-        s.startsWith(("three")) -> '3'
-        s.startsWith("four") ->  '4'
-        s.startsWith("five") ->  '5'
-        s.startsWith("six") ->  '6'
-        s.startsWith("seven") ->  '7'
-        s.startsWith("eight") ->  '8'
-        s.startsWith("nine") ->  '9'
-        else -> findFirstNumber(s.drop(1))
-    }
-
-fun findLastNumber(s: String): Char =
-    when {
-        s.last().isDigit() -> s.last()
-        s.endsWith("one") -> '1'
-        s.endsWith("two") -> '2'
-        s.endsWith(("three")) -> '3'
-        s.endsWith("four") ->  '4'
-        s.endsWith("five") ->  '5'
-        s.endsWith("six") ->  '6'
-        s.endsWith("seven") ->  '7'
-        s.endsWith("eight") ->  '8'
-        s.endsWith("nine") ->  '9'
-        else -> findLastNumber(s.dropLast(1))
-    }
-
+fun mapNumbers(s: String): String {
+    return s.replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "4")
+        .replace("five", "5e")
+        .replace("six", "6")
+        .replace("seven", "7")
+        .replace("eight", "8t")
+        .replace("nine", "9e")
+}
